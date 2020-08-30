626b4e1540948ec531ce18e0cb2a598e36956ab0
tukideng <33deng@riseup.net>
2020-08-30 15:52:45 +0800

    detail

---
+++ src/Pages/Detail/components/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    detailsssss
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/var.styl'
+
+</style>
